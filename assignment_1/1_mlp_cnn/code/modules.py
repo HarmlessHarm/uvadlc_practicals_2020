ea01d8916fc53a8564469fbd6c23c5528c8ed446
Harm Manders <harm@confocal.nl>
2020-11-08 17:03:50 +0100

    Fixed Cross Entropy

--- assignment_1/1_mlp_cnn/code/modules.py
+++ assignment_1/1_mlp_cnn/code/modules.py
@@ -59,7 +59,8 @@ class LinearModule(object):
         dx = dout @ self.params['weight'].T
         
         self.grads['weight'] = self.x.T @ dout
-        self.grads['bias'] = np.ones((1, dout.shape[0])) @ dout
+        batch_size = dout.shape[0]
+        self.grads['bias'] = np.ones((1, batch_size)) @ dout
 
         # print('\nIN BACKWARD\n')
         # print(self.params['bias'].shape)
@@ -108,6 +109,7 @@ class SoftMaxModule(object):
           out: output of the module
     
         Hint: You can store intermediate variables inside the object. They can be used in backward pass computation.
+        To stabilize computation you should use the so-called Max Trick - https://timvieira.github.io/blog/post/2014/02/11/exp-normalize-trick/
         """
         self.x = x
         out = self.softmax(x)
@@ -127,19 +129,20 @@ class SoftMaxModule(object):
         m, n = self.x.shape
         softmax_mat = self.softmax(self.x)
         
-        dx = np.zeros((m, n))
-        for i, sample in enumerate(softmax_mat):
-            # soft = self.softmax(sample)
-            soft_grad = self.softmax_grad(sample)
-            dx[i] = dout[i] @ soft_grad
+        # dx = np.zeros((m, n))
+        # for i, sample in enumerate(softmax_mat):
+        #     # soft = self.softmax(sample)
+        #     soft_grad = self.softmax_grad(sample)
+        #     dx[i] = dout[i] @ soft_grad
         
 
         # Example with einsum notation
         # Reference: https://themaverickmeerkat.com/2019-10-23-Softmax/
-        # t1 = np.einsum('ij,ik->ijk', softmax_mat, softmax_mat)
-        # t2 = np.einsum('ij,jk->ijk', softmax_mat, np.eye(n, n))
-        # dSm = t2 - t1
-        # dx = np.einsum('ijk,ik->ij', dSm, dout)
+        
+        t1 = np.einsum('ij,ik->ijk', softmax_mat, softmax_mat)
+        t2 = np.einsum('ij,jk->ijk', softmax_mat, np.eye(n, n))
+        dSm = t2 - t1
+        dx = np.einsum('ijk,ik->ij', dSm, dout)
 
         return dx
 
@@ -159,7 +162,8 @@ class CrossEntropyModule(object):
           out: cross entropy loss
         """
 
-        out = - np.sum(y * np.log(x))
+        out = np.mean(-np.sum(y * np.log(x), axis=1))
+        # out = -np.log(x[np.arange(x.shape[0]), y.argmax(1)]).mean()
         return out
     
     def backward(self, x, y):
@@ -171,8 +175,8 @@ class CrossEntropyModule(object):
         Returns:
           dx: gradient of the loss with the respect to the input x.
         """
-        dx = - y / x
-        
+        # dx = - y / x
+        dx = -(y / x) / y.shape[0]
         return dx
 
 
@@ -193,8 +197,11 @@ class ELUModule(object):
         Hint: You can store intermediate variables inside the object. They can be used in backward pass computation.
         """
         self.x = x
+        # print('IN ELU')
+        # print(x.shape)
+        # print("max",np.max(x))
+        # print("min",np.min(x))
         out = np.where( x < 0, np.exp(x) - 1, x)
-        
         return out
     
     def backward(self, dout):
@@ -206,5 +213,40 @@ class ELUModule(object):
           dx: gradients with respect to the input of the module
         """
         dx = dout * np.where( self.x < 0, np.exp(self.x), 1)
+
+        return dx
+
+
+class ReLUModule(object):
+    """
+    ReLU activation module.
+    """
+    
+    def forward(self, x):
+        """
+        Forward pass.
+
+        Args:
+          x: input to the module
+        Returns:
+          out: output of the module
+
+        Hint: You can store intermediate variables inside the object. They can be used in backward pass computation.
+        """
+        self.x = x
+        # print('IN ELU')
+        # print(x.shape)
+        out = np.where( x < 0, 0, x)
+        return out
+    
+    def backward(self, dout):
+        """
+        Backward pass.
+        Args:
+          dout: gradients of the previous module
+        Returns:
+          dx: gradients with respect to the input of the module
+        """
+        dx = dout * np.where( self.x < 0, 0, 1)
 
         return dx
