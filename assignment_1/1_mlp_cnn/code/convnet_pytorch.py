ffce619d6a6b0d1a8eefc5831afdb64f27833fe1
Harm Manderse <harmmanders@gmail.com>
2020-11-15 13:09:07 +0100

    Created Convnet

--- assignment_1/1_mlp_cnn/code/convnet_pytorch.py
+++ assignment_1/1_mlp_cnn/code/convnet_pytorch.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import torch.nn as nn
 
 class ConvNet(nn.Module):
     """
@@ -26,14 +27,24 @@ class ConvNet(nn.Module):
         TODO:
         Implement initialization of the network.
         """
-        
-        ########################
-        # PUT YOUR CODE HERE  #
-        #######################
-        raise NotImplementedError
-        ########################
-        # END OF YOUR CODE    #
-        #######################
+        super(ConvNet, self).__init__()
+
+        act_fn = nn.ReLU()
+
+        self.net_modules = (
+            nn.Conv2d(n_channels, 64, kernel_size=3, padding=1), # Conv0
+            SimplePreActBlock(nn.ReLU(), 64, double=False, downsample=False), # PreAct1
+            SimplePreActBlock(nn.ReLU(), 64, 128),  # conv1, maxpool1, PreAct2_a, PreAct2_b
+            SimplePreActBlock(nn.ReLU(), 128, 256), # conv2, maxpool2, PreAct3_a, PreAct3_b
+            SimplePreActBlock(nn.ReLU(), 256, 512), # conv3, maxpool3, PreAct4_a, PreAct4_b
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1), # maxpool4
+            SimplePreActBlock(nn.ReLU(), 512, downsample=False), # PreAct5_a PreAct5_b
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1), # maxpool5
+            View([512]), # Reshape output of conv to batch_size x 512
+            nn.Linear(in_features=512, out_features=n_classes), # linear
+        )
+
+        self.net = nn.Sequential(*self.net_modules)
     
     def forward(self, x):
         """
@@ -49,12 +60,80 @@ class ConvNet(nn.Module):
         Implement forward pass of the network.
         """
         
-        ########################
-        # PUT YOUR CODE HERE  #
-        #######################
-        raise NotImplementedError
-        ########################
-        # END OF YOUR CODE    #
-        #######################
+        # out = self.net(x)
+        # out = x
+        # for mod in self.net_modules:
+        #     print("\n-----\n")
+        #     print("Module:", mod)
+        #     print("in:",out.shape)
+        #     if type(mod) == nn.Linear:
+        #         print("weight:", mod.weight.shape)
+        #     out = mod(out)
+
+        #     print("out:", out.shape)
         
+        out = self.net(x)
+
         return out
+
+
+class SimplePreActBlock(nn.Module):
+
+    def __init__(self, act_fn, c_in, c_out=None, double=True, downsample=True):
+        super(SimplePreActBlock, self).__init__()
+
+        
+        if not downsample:
+            c_out = c_in
+
+        self.downsample = nn.Sequential(
+            nn.Conv2d(c_in, c_out, kernel_size=1, stride=1, padding=0),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        ) if downsample else None
+
+        modules = (
+            nn.BatchNorm2d(c_out),
+            nn.ReLU(),
+            nn.Conv2d(c_out, c_out, kernel_size=3, stride=1, padding=1)
+        )
+
+        if double:
+            modules = (
+                nn.BatchNorm2d(c_out),
+                nn.ReLU(),
+                nn.Conv2d(c_out, c_out, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm2d(c_out),
+                nn.ReLU(),
+                nn.Conv2d(c_out, c_out, kernel_size=3, stride=1, padding=1)
+            )
+
+        self.net = nn.Sequential(*modules)
+
+    def forward(self, x):
+
+        # First downsample x,y dims and upscale channel dim
+        if self.downsample != None:
+            x = self.downsample(x)
+
+        # Then apply PreAct module
+        z = self.net(x)
+        
+        
+
+        return z + x
+
+
+class View(nn.Module):
+    """
+    Reshapes the the input tensor
+
+    Args:
+        shape: tuple of shape of tensor not including batch size
+    """
+    def __init__(self, shape):
+        super(View, self).__init__()
+        self.shape = shape
+        
+
+    def forward(self, x):
+        return x.view(x.shape[0], *self.shape)
